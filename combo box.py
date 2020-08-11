ec61e8a45bd51ee7ca92444d306e0e5234084266
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2020-08-11 18:32:52 +0200

    combo box con seleccion aleatoria
    combo box con seleccion aleatoria

--- combo box.py
+++ combo box.py
@@ -1,19 +1,15 @@
 from tkinter import *
-
 from tkinter.ttk import *
-
+import random
 window = Tk()
-
-window.title("Practicando!")
-
+window.title("Welcome to LikeGeeks app")
 window.geometry('350x200')
-
 combo = Combobox(window)
-
-combo['values']= (1, 2, 3, 4, 5, "Text")
-
-combo.current(1) #set the selected item
-
+valores = [1, 2, 3, 4, 5, "Text"]
+hola = random.choice(valores)
+combo['values']= valores
+combo.current(hola) #set the random value
 combo.grid(column=0, row=0)
-
+lbl = Label(text="Default"+combo.get())
+lbl.grid(column=0, row=3)
 window.mainloop()
